e69d042aafdb259231cfab9f735905484cb97736
60 <chany229@sina.com>
2014-10-14 10:43:20 +0800

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-
-  resources :lessons
-
-  resources :courses
-
+  namespace :accounts do
+    resources :lessons
+    resources :courses
+  end
+  namespace :admin do
+    resources :lessons
+    resources :courses
+  end
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
