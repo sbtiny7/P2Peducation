cb52bbeaa0e07611b35e183247b4d92b64af299c
60 <chany229@sina.com>
2014-10-14 17:58:42 +0800

    .

---
+++ config/initializers/azure.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.azure_credentials = {
+    storage_account_name: "portalvhdsvt7pcctrrrfv3",
+    storage_access_key: "dN2RM5zC1NpZWBmcPsP/rm1UD460kDcbNSdEFjbPywzSWQQtJy/OUYXVVKh6E4TE7E8kw5xxhiIWaa2Udtd7ag=="
+  }
+  config.azure_container = "avatar"
+  config.azure_host = "blob.core.chinacloudapi.cn"
+end
