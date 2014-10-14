053bba98a8029126d3d34657b50681c4500c2880
60 <chany229@sina.com>
2014-10-14 15:52:45 +0800

    .

--- app/controllers/accounts/sessions_controller.rb
+++ app/controllers/accounts/sessions_controller.rb
@@ -1,2 +1,33 @@
 class Accounts::SessionsController < Devise::SessionsController
+    before_filter :set_request_format, :only => [:new]
+    private
+    def require_no_authentication
+        Rails.logger.info "come here "
+        respond_to do |format|
+            format.json {
+                session["warden.user.user.key"] = nil
+            }
+            format.html {
+                session["warden.user.user.key"] = nil
+            }
+            format.js {
+                session["warden.user.user.key"] = nil
+            }
+        end
+    end
+
+    def set_request_format
+        if is_mobile_device?
+            unless request.format.to_s.include? "json"
+                request.format = :mobile
+            end
+        end
+    end
+    def is_mobile_device?
+        !!mobile_device
+    end
+
+    def mobile_device
+        request.headers['X_MOBILE_DEVICE']
+    end
 end
