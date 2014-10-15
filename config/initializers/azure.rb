30bccd69ad3e5aeeacade5820262eac0a45b0a18
60 <chany229@sina.com>
2014-10-15 13:40:02 +0800

    .

--- config/initializers/azure.rb
+++ config/initializers/azure.rb
@@ -4,5 +4,5 @@ CarrierWave.configure do |config|
     storage_access_key: "dN2RM5zC1NpZWBmcPsP/rm1UD460kDcbNSdEFjbPywzSWQQtJy/OUYXVVKh6E4TE7E8kw5xxhiIWaa2Udtd7ag=="
   }
   config.azure_container = "jyimages"
-  config.azure_host = "blob.core.chinacloudapi.cn"
+  config.azure_host = "http://portalvhdsvt7pcctrrrfv3.blob.core.chinacloudapi.cn"
 end
