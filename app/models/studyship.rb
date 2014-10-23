fc2ff3a3bf3099d17bcd86d29eb8d72dc2fd9a4a
60 <chany229@sina.com>
2014-10-23 14:28:12 +0800

    .

--- app/models/studyship.rb
+++ app/models/studyship.rb
@@ -1,4 +1,28 @@
 class Studyship < ActiveRecord::Base
     belongs_to :student, :class_name => "User"
     belongs_to :course
+
+    after_create :generate_token
+
+    def teacher
+        self.course.user
+    end
+
+    def teacher_id
+        self.course.user_id
+    end
+
+    def ar_code_str
+        "/api/study/study?token=#{self.token}&course_id=#{self.course_id}&student_id=#{self.student_id}&teacher_id=#{self.teacher_id}"
+    end
+
+    protected
+    def generate_token
+        begin
+            self.token = Message.faye_rand
+            self.save!
+        rescue ActiveRecord::RecordNotUnique => e
+            retry
+        end
+    end
 end
--- db/migrate/20141010065705_create_studyships.rb
+++ db/migrate/20141010065705_create_studyships.rb
@@ -3,6 +3,7 @@ class CreateStudyships < ActiveRecord::Migration
     create_table :studyships do |t|
       t.integer :student_id
       t.integer :course_id
+      t.string  :token
 
       t.timestamps
     end
