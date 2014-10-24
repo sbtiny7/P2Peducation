3f9e3353d79b0328010cf30c47c312ddcf66cfb2
60 <chany229@sina.com>
2014-10-24 17:13:06 +0800

    .

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,9 +2,13 @@ class Course < ActiveRecord::Base
     TYPES = %w(ONLINE OFFLINE)
     CATEGORIES = %w(实用技能 兴趣爱好)
     DEFAULT_IMG_PATH = "/assets/temp.jpg"
+
     mount_uploader :image, ImageUploader
     mount_uploader :tmp_image, TempUploader
     acts_as_commentable :chat, :qa # commentable.chat_comments, commentable.qa_comments
+
+    attr_accessor :start_time_date, :start_time_hour, :start_time_min, :end_time_date, :end_time_hour, :end_time_min
+
     belongs_to :user
     has_many :lessons
     has_many :studyships
