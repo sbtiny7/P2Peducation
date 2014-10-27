adf22027730a7fe538a32b89c3297ece38810e3b
60 <chany229@sina.com>
2014-10-27 11:03:12 +0800

    .

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -1,5 +1,5 @@
 $(function(){
-    
+
     $('.upload_image:not(input)').mouseover(function(){
         $('icon.upload_image').show();
     }).mouseout(function(){
@@ -10,25 +10,24 @@ $(function(){
         on_upload_image($(this));
     })
 
-    function on_upload_image(sender) {
-        if( !sender.val().match( /.jpg|.jpeg|.gif|.png|.bmp/i ) ){
-            alert('图片格式无效！');
-            return false;
-        }
-        var img = sender.parent().children('img')[0];
-        var ie_preview_wrapper = sender.parent().children('.ie_preview_wrapper');
-        var ie_preview = ie_preview_wrapper.children('.ie_preview');
-        if( sender[0].files &&  sender[0].files[0] ){
-            img.src = window.URL.createObjectURL(sender[0].files[0]);
-        } else if( ie_preview[0].filters ) {
-            sender.parent().children('img').hide();
-            sender.select();
-            sender.blur();
-            var path = document.selection.createRange().text;
-            ie_preview[0].filters.item('DXImageTransform.Microsoft.AlphaImageLoader').src = path;
-        }
-    }
-
-
     $('input.datetime').datepicker({ constrainInput: false })
 })
+
+function on_upload_image(sender) {
+    if( !sender.val().match( /.jpg|.jpeg|.gif|.png|.bmp/i ) ){
+        alert('图片格式无效！');
+        return false;
+    }
+    var img = sender.parent().children('img')[0];
+    var ie_preview_wrapper = sender.parent().children('.ie_preview_wrapper');
+    var ie_preview = ie_preview_wrapper.children('.ie_preview');
+    if( sender[0].files &&  sender[0].files[0] ){
+        img.src = window.URL.createObjectURL(sender[0].files[0]);
+    } else if( ie_preview[0].filters ) {
+        sender.parent().children('img').hide();
+        sender.select();
+        sender.blur();
+        var path = document.selection.createRange().text;
+        ie_preview[0].filters.item('DXImageTransform.Microsoft.AlphaImageLoader').src = path;
+    }
+}
