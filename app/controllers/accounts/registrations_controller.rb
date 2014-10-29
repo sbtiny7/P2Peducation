3c3bbdd31b8f09379422575e9716024f181196a8
60 <chany229@sina.com>
2014-10-29 09:41:44 +0800

    .

--- app/controllers/accounts/registrations_controller.rb
+++ app/controllers/accounts/registrations_controller.rb
@@ -1,4 +1,9 @@
 class Accounts::RegistrationsController < Devise::RegistrationsController
-  prepend_before_filter :require_no_authentication, only: [ :new, :create, :cancel ]
-  prepend_before_filter :authenticate_scope!, only: [:edit, :update, :destroy]
+    prepend_before_filter :require_no_authentication, only: [ :new, :create, :cancel ]
+    prepend_before_filter :authenticate_scope!, only: [:edit, :update, :destroy]
+
+    def edit
+        super
+        render :layout => :accounts
+    end
 end
