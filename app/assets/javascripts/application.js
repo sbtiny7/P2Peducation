f767b9b23922b27fa0f1e81f94d15f0416c84e4f
60 <chany229@sina.com>
2014-10-29 16:10:33 +0800

    .

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,3 +33,9 @@ $(function(){
 jQuery.validator.addMethod("notEqual", function(value, element, param) {
   return this.optional(element) || value != param;
 }, "Please specify a different (non-default) value");
+jQuery.validator.addMethod("isPhone", function(value, element) {       
+    var length = value.length;   
+    var mobile = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+    var tele = /^\d{3,4}-?\d{7,9}$/
+    return this.optional(element) || (length == 11 && mobile.test(value)) || tele.test(value);       
+}, "请填写正确的电话号码（固话含区号）");
