f20642e891acd87822f596ddde631e5861cf970c
60 <chany229@sina.com>
2014-10-29 09:53:09 +0800

    .

--- app/controllers/accounts/registrations_controller.rb
+++ app/controllers/accounts/registrations_controller.rb
@@ -2,8 +2,13 @@ class Accounts::RegistrationsController < Devise::RegistrationsController
     prepend_before_filter :require_no_authentication, only: [ :new, :create, :cancel ]
     prepend_before_filter :authenticate_scope!, only: [:edit, :update, :destroy]
 
-    def edit
-        super
-        render :layout => :accounts
+    layout :set_layout
+
+    private
+
+    def set_layout
+        if action_name == "edit"
+            return 'accounts'
+        end
     end
 end
