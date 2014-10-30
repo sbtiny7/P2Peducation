836244a497ff93288d445967fa29e9e6e1ab6799
Pengfei Shi <spfzzz@163.com>
2014-10-30 18:26:45 +0800

    update index page of js files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,30 +12,30 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require json2
-//= require kindeditor
-//= require make_ie_happy
-//= require jquery.ui.all
-//= require jquery.ui.datepicker
-//= require jquery-ui-timepicker-addon
-//= require jquery.ui.datepicker-zh-CN.js.js
-//= require jquery-ui-timepicker-zh-CN.js
-//= require jquery.ui.timepicker
-//= require jquery.placeholder.js
-//= require jquery.validate
-//= require additional-methods.min.js
-//= require jquery.jcrop
-//= require bootstrap-sprockets
+// require json2
+// require kindeditor
+// require make_ie_happy
+// require jquery.ui.all
+// require jquery.ui.datepicker
+// require jquery-ui-timepicker-addon
+// require jquery.ui.datepicker-zh-CN.js.js
+// require jquery-ui-timepicker-zh-CN.js
+// require jquery.ui.timepicker
+// require jquery.placeholder.js
+// require jquery.validate
+// require additional-methods.min.js
+// require jquery.jcrop
+// require bootstrap-sprockets
 // require_tree .
-$(function(){
-  $('input, textarea').placeholder();
-})
-jQuery.validator.addMethod("notEqual", function(value, element, param) {
-  return this.optional(element) || value != param;
-}, "Please specify a different (non-default) value");
-jQuery.validator.addMethod("isPhone", function(value, element) {       
-    var length = value.length;   
-    var mobile = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
-    var tele = /^\d{3,4}-?\d{7,9}$/
-    return this.optional(element) || (length == 11 && mobile.test(value)) || tele.test(value);       
-}, "请填写正确的电话号码（固话含区号）");
+//$(function(){
+//  $('input, textarea').placeholder();
+//})
+//jQuery.validator.addMethod("notEqual", function(value, element, param) {
+//  return this.optional(element) || value ! param;
+//}, "Please specify a different (non-default) value");
+//jQuery.validator.addMethod("isPhone", function(value, element) {
+//    var length = value.length;
+//    var mobile = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+//    var tele = /^\d{3,4}-?\d{7,9}$/
+//    return this.optional(element) || (length == 11 && mobile.test(value)) || tele.test(value);
+//}, "请填写正确的电话号码（固话含区号）");
