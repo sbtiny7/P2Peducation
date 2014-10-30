c189284b2bbef690ca34605303679f51be12fa6b
Pengfei Shi <spfzzz@163.com>
2014-10-30 18:28:24 +0800

    update precompile config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( home.css accounts.css accounts.js )
+# Rails.application.config.assets.precompile += %w( home.css accounts.css accounts.js )
+Rails.application.config.assets.precompile << Proc.new { |path|
+  path =~ /\.(css|js)\z/ ? true : false
+}
