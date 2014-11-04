926b38ed0e66cc72c14b6c1c7dfd697cf35a55a6
neal <guyanbiao@gmail.com>
2014-11-04 16:39:00 +0800

    store previous path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,13 @@ class ApplicationController < ActionController::Base
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
-    after_action  :clean_notice
+    after_action  :clean_notice, :store_location
+
 
     protected
+    def after_sign_in_path_for(resource)
+        session[:previous_url] || root_path
+    end
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.for(:sign_up) << :username
@@ -29,5 +33,16 @@ class ApplicationController < ActionController::Base
 
     def clean_notice
         flash[:notice] = nil
+    end
+
+
+
+
+    def store_location
+        black_list = ["/users/sign_in", "/users/sign_up", "/users/password/new",
+            "/users/password/edit", "/users/confirmation", "/users/sign_out"]
+        if !(black_list.include?(request.path) or request.xhr?)
+            session[:previous_url] = request.fullpath 
+        end
     end
 end
