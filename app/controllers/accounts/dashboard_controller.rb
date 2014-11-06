a852c054becc1769523723274bf527a1bbccb3aa
neal <guyanbiao@gmail.com>
2014-11-06 15:07:17 +0800

    add course

--- app/controllers/accounts/dashboard_controller.rb
+++ app/controllers/accounts/dashboard_controller.rb
@@ -1,19 +1,16 @@
 class Accounts::DashboardController <  ApplicationController
   before_action :authenticate_user!
   layout "accounts"
-  def student
-    @courses = (1..11).to_a
-  end
   def learning
-    @courses = (1..11).to_a
+    @courses = Course.limit(7)
     render "union_courses.html.erb"
   end
   def pending
-    @courses = (1..7).to_a
+    @courses = Course.limit(3)
     render "union_courses.html.erb"
   end
   def favorite
-    @courses = (1..3).to_a
+    @courses = Course.limit(10)
     render "union_courses.html.erb"
   end
   def incoming
