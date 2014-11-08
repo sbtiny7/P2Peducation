4a56a30047f3b9083538443e4a701a7f085dba3b
emYOUR NAME/em <emYOUR EMAIL ADDRESS/em>
2014-11-08 17:53:57 +0800

    make login page

--- app/controllers/accounts/sessions_controller.rb
+++ app/controllers/accounts/sessions_controller.rb
@@ -19,4 +19,14 @@ class Accounts::SessionsController < Devise::SessionsController
     def mobile_device
         request.headers['X_MOBILE_DEVICE']
     end
+
+    module DeviseHelper
+      def devise_error_messages1!
+        resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+      end
+
+      def devise_error_messages2!
+        resource.errors.full_messages.map { |msg| content_tag(:p, msg) }.join
+      end
+    end
 end
