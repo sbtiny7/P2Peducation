a6a660556dd29933eb1319d1dd9bd51ccd05a17e
Your Name <you@example.com>
2014-11-12 10:48:26 +0800

    add expired tips

--- app/helpers/accounts/orders_helper.rb
+++ app/helpers/accounts/orders_helper.rb
@@ -1,2 +1,9 @@
 module Accounts::OrdersHelper
+    def expired_duration
+        display_time Settings.expired_duration
+    end
+
+    def display_time(sec)
+        (sec / 60).to_s + "分钟"
+    end
 end
