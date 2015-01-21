9535567b19e9fac59f6d4d5dfe9115c589a3b853
neal <guyanbiao@gmail.com>
2015-01-21 10:36:49 +0800

    set laylout

--- app/controllers/accounts/registrations_controller.rb
+++ app/controllers/accounts/registrations_controller.rb
@@ -2,7 +2,6 @@ class Accounts::RegistrationsController < Devise::RegistrationsController
     prepend_before_filter :require_no_authentication, only: [ :new, :create, :cancel ]
     prepend_before_filter :authenticate_scope!, only: [:edit, :update, :destroy]
 
-    layout :set_layout
 
     private
 
